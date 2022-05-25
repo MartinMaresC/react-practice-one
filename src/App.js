974d38ce6083cd1aae5fcf8baa11b747a1e598b7
Martin <martinmarescr@gmail.com>
2022-05-24 23:37:56 -0500

    Se agrega libreria uuid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, {useState, useRef, useEffect} from "react";
 import TodoList from "./TodoList";
+import {v4 as uuidv4} from 'uuid'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(()=>{
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) {
+      setTodos(storedTodos)
+    }
+  }, [])
+
+  useEffect(()=> {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos]);
+
+  function toogleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name ==='') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id:uuidv4(), name:name, complete:false}]
+    })
+    console.log(name)
+    todoNameRef.current.value = null
+  }
+
   return (
     <>
-    <TodoList />
-    <input type="text" />
-    <button>Add To do</button>
+    <TodoList todos = {todos} toogleTodo={toogleTodo}/>
+    <input ref={todoNameRef} type="text" />
+    <button onClick={handleAddTodo}>Add To do</button>
     <button>Clear Completed To dos</button>
     <div>0 left to do list</div>
     </>
